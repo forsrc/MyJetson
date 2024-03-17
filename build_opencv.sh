41ad5e8f206d636dc5fce2bd65b2fd51fce32551
forsrc <forsrc@gmail.com>
2024-03-17 14:01:38 +0900

    Create build_opencv.sh

---
+++ build_opencv.sh
@@ -0,0 +1,51 @@
+version="4.6.0"
+
+curl -L https://github.com/opencv/opencv/archive/${version}.zip -o opencv-${version}.zip
+curl -L https://github.com/opencv/opencv_contrib/archive/${version}.zip -o opencv_contrib-${version}.zip
+unzip opencv-${version}.zip
+unzip opencv_contrib-${version}.zip
+
+
+mkdir -p  build
+cd build
+
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_INSTALL_PREFIX=/usr/local \
+-D WITH_TBB=ON \
+-D ENABLE_FAST_MATH=1 \
+-D CUDA_FAST_MATH=1 \
+-D WITH_CUBLAS=1 \
+-D WITH_CUDA=ON \
+-D BUILD_NEW_PYTHON_SUPPORT=ON \
+-D HAVE_opencv_python3=ON \
+-D BUILD_opencv_python3=ON \
+-D BUILD_opencv_python2=OFF \
+-D BUILD_opencv_cudacodec=OFF \
+-D WITH_CUDNN=ON \
+-D OPENCV_DNN_CUDA=ON \
+-D CUDA_ARCH_BIN=7.2 \
+-D WITH_V4L=ON \
+-D WITH_QT=OFF \
+-D WITH_OPENGL=ON \
+-D WITH_GSTREAMER=ON \
+-D OPENCV_GENERATE_PKGCONFIG=ON \
+-D OPENCV_PC_FILE_NAME=opencv.pc \
+-D OPENCV_ENABLE_NONFREE=ON \
+-D OPENCV_PYTHON3_INSTALL_PATH=/usr/local/lib/python3.8/dist-packages \
+-D PYTHON_EXECUTABLE=/usr/bin/python \
+-D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib-4.5.4/modules \
+-D INSTALL_PYTHON_EXAMPLES=ON \
+-D INSTALL_C_EXAMPLES=ON \
+-D PYTHON_DEFAULT_EXECUTABLE=`which python3` \
+-D BUILD_EXAMPLES=ON ..
+
+
+
+nproc
+make -j8
+sudo make install
+
+
+
+#sudo /bin/bash -c 
