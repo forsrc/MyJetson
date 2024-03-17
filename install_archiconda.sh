37a9b78b81fd39eb1615972169c9260607fd426d
forsrc <forsrc@gmail.com>
2024-03-17 22:36:50 +0900

    Create install_archiconda.sh

---
+++ install_archiconda.sh
@@ -0,0 +1,7 @@
+
+
+#https://github.com/Archiconda/build-tools/releases
+
+wget https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh
+
+sh Archiconda3-0.2.3-Linux-aarch64.sh
