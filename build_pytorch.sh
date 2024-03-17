61b895a21c138ac9ec34c1b9b205bed66f127776
forsrc <forsrc@gmail.com>
2024-03-17 22:44:39 +0900

    Create build_pytorch.sh

---
+++ build_pytorch.sh
@@ -0,0 +1,42 @@
+
+VER=2.2.1
+
+git clone --recursive --branch v${VER} http://github.com/pytorch/pytorch
+
+cd pytorch
+
+sudo apt-get install python3-pip cmake libopenblas-dev libopenmpi-dev
+
+
+conda create py310 python=3.10
+conda activate py310 
+
+pip3 install -r requirements.txt
+pip3 install scikit-build ninja cmake 
+
+
+export USE_NCCL=0
+export USE_DISTRIBUTED=1
+export USE_QNNPACK=0
+export USE_PYTORCH_QNNPACK=0
+
+# Orin is based on Ampere Achitecture
+export TORCH_CUDA_ARCH_LIST="7.2;8.7"
+
+export PYTORCH_BUILD_VERSION=${VER}
+export PYTORCH_BUILD_NUMBER=1
+
+python3 setup.py bdist_wheel
+
+pip install dist/torch-${VER}-cp310-cp310-linux_aarch64.whl
+
+
+####
+#>>> import torch
+#>>> torch.cuda.is_available()
+#True
+#>>> torch.cuda.get_device_name(0)
+#'Orin'
+#>>> device = torch.device('cuda', 0)
+#>>> torch.rand(4).to(device)
+#tensor([0.7315, 0.2583, 0.2840, 0.7977], device='cuda:0')
