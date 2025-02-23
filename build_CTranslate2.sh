2b598b4b049b70dd00ad97d17175c27618f5434e
forsrc <forsrc@gmail.com>
2025-02-23 11:06:47 +0900

    Create build_CTranslate2.sh

---
+++ build_CTranslate2.sh
@@ -0,0 +1,19 @@
+git clone https://github.com/OpenNMT/CTranslate2.git
+cd CTranslate2
+
+
+mkdir build
+cd build
+
+cmake -DWITH_CUDA=ON -Bbuild_folder -DWITH_MKL=OFF ..
+cmake —build build_folder
+
+cmake -Bbuild_folder -DWITH_MKL=OFF -DOPENMP_RUNTIME=NONE -DWITH_CUDA=ON -DWITH_CUDNN=ON
+cmake —build build_folder
+
+cd build_folder
+make -j8
+sudo make install
+
+python -c "import ctranslate2;print(ctranslate2.get_cuda_device_count())"
+
